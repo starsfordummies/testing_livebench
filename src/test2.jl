9fb6859d714eef5132089d4d3454488ff6e23faa
Stefano Carignano <stefano.carignano@fqa.ub.edu>
2025-01-16 23:48:20 +0100

    ghp

---
+++ src/test2.jl
@@ -0,0 +1,23 @@
+using BenchmarkTools
+using ITensors, ITensorMPS
+
+function over_lap(L)
+ss = siteinds("S=1/2", L)
+
+psi = random_mps(ss, linkdims=100)
+phi = random_mps(ss, linkdims=100)
+
+return inner(psi,phi)
+
+
+suite = BenchmarkGroup()
+
+suite["fib"] = BenchmarkGroup(["tag1", "tag2"])
+
+suite["fib"][10] = @benchmarkable over_lap(10)
+suite["fib"][20] = @benchmarkable over_lap(20)
+
+tune!(suite)
+results = run(suite, verbose = true)
+
+BenchmarkTools.save("output.json", median(results))
