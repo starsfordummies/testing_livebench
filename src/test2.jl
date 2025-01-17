2637109e5fc3d3b55c0df9da4f85fe75e02f96ec
Stefano Carignano <70697517+starsfordummies@users.noreply.github.com>
2025-01-17 10:23:28 +0100

    fix overlap, better arrange

--- src/test2.jl
+++ src/test2.jl
@@ -5,7 +5,7 @@ function over_lap(L)
     ss = siteinds("S=1/2", L)
 
     psi = random_mps(ss, linkdims=100)
-    o = random_mps(ss) + random_mpo(ss)
+    o = random_mpo(ss) + random_mpo(ss)
 
     psi = apply(o, psi)
     phi = random_mps(ss, linkdims=100)
@@ -17,10 +17,10 @@ end
 
 suite = BenchmarkGroup()
 
-suite["ITensors overlap"] = BenchmarkGroup(["tag1", "tag2"])
+suite["ITensors <phi|O|psi>"] = BenchmarkGroup(["ITensors", "overlaps"])
 
-suite["L=10, chi=100"][10] = @benchmarkable over_lap(10)
-suite["L=20, chi=100"][20] = @benchmarkable over_lap(20)
+suite["ITensors <phi|O|psi>"]["L=10, chi=100"] = @benchmarkable over_lap(10)
+suite["ITensors <phi|O|psi>"]["L=20, chi=100"] = @benchmarkable over_lap(20)
 
 tune!(suite)
 results = run(suite, verbose = true)
