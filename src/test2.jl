a98ea4410c23614e79e11b876eb8f8e08df3b5fa
Stefano Carignano <70697517+starsfordummies@users.noreply.github.com>
2025-01-17 10:09:00 +0100

    try overlap itensors

--- src/test2.jl
+++ src/test2.jl
@@ -2,19 +2,25 @@ using BenchmarkTools
 using ITensors, ITensorMPS
 
 function over_lap(L)
-ss = siteinds("S=1/2", L)
+    ss = siteinds("S=1/2", L)
 
-psi = random_mps(ss, linkdims=100)
-phi = random_mps(ss, linkdims=100)
+    psi = random_mps(ss, linkdims=100)
+    o = random_mps(ss) + random_mpo(ss)
 
-return inner(psi,phi)
+    psi = apply(o, psi)
+    phi = random_mps(ss, linkdims=100)
+
+    return inner(psi,phi)
+end
+
+
 
 suite = BenchmarkGroup()
 
-suite["fib"] = BenchmarkGroup(["tag1", "tag2"])
+suite["ITensors overlap"] = BenchmarkGroup(["tag1", "tag2"])
 
-suite["fib"][10] = @benchmarkable over_lap(10)
-suite["fib"][20] = @benchmarkable over_lap(20)
+suite["L=10, chi=100"][10] = @benchmarkable over_lap(10)
+suite["L=20, chi=100"][20] = @benchmarkable over_lap(20)
 
 tune!(suite)
 results = run(suite, verbose = true)
