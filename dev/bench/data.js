4d80ea1855401988cac69d0deb0a15dd8250b5e1
github-action-benchmark <github@users.noreply.github.com>
2025-01-19 16:31:19 +0000

    add Julia benchmark result (julia) benchmark result for 0986a6801a6c1333fc0877a63460d679028b6ea5

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1737304279757,
+  "repoUrl": "https://github.com/starsfordummies/testing_livebench",
+  "entries": {
+    "Julia benchmark result": [
+      {
+        "commit": {
+          "author": {
+            "email": "70697517+starsfordummies@users.noreply.github.com",
+            "name": "Stefano Carignano",
+            "username": "starsfordummies"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0986a6801a6c1333fc0877a63460d679028b6ea5",
+          "message": "use master",
+          "timestamp": "2025-01-19T17:26:32+01:00",
+          "tree_id": "586e39f49bfab1c8561174392e6180fd431d24d8",
+          "url": "https://github.com/starsfordummies/testing_livebench/commit/0986a6801a6c1333fc0877a63460d679028b6ea5"
+        },
+        "date": 1737304279470,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "<phi|O|psi>/L=20/aa/Library1",
+            "value": 15179504,
+            "unit": "ns",
+            "extra": "gctime=1884799.5\nmemory=23911152\nallocs=55713\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "<phi|O|psi>/L=20/aa/Library2",
+            "value": 85948694,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=58910072\nallocs=1158164\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "<phi|O|psi>/L=10/aa/Library1",
+            "value": 4862710,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=8709904\nallocs=25857\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "<phi|O|psi>/L=10/aa/Library2",
+            "value": 23149428,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=12566408\nallocs=252832\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
